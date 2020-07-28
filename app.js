7a3e61d8cccb27de89ae8b3c27eb1d839add719d
Tanvir Tanjum Shourav <39719570+tanvirtanjum@users.noreply.github.com>
2020-07-29 02:14:12 +0600

    T2M3F2

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var employee 		= require('./controller/employee');
 var addemployee = require('./controller/addemployee');
 var allemployeelist = require('./controller/allemployeelist');
 var update = require('./controller/update');
+var myprofile = require('./controller/myprofile');
 
 var app 		= express();
 
@@ -23,6 +24,7 @@ app.use('/employee', employee);
 app.use('/admin/addemployee', addemployee);
 app.use('/admin/allemployeelist', allemployeelist);
 app.use('/admin/allemployeelist/update', update);
+app.use('/employee/myprofile', myprofile);
 
 app.get('/', function(req, res)
 {
--- controller/employee.js
+++ controller/employee.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res)
 	res.render('employee/index');
 });
 
+router.post('/', function(req, res)
+{
+	if(req.body.hasOwnProperty("mp"))
+	{
+		res.redirect('employee/myprofile');
+	}
+
+	else if (req.body.hasOwnProperty("up"))
+	{
+		res.redirect('employee/myprofile');
+	}
+
+});
+
 module.exports = router;
