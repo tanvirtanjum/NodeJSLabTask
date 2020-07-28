e09cae078f0cdc62bee807f702851a00ad4c55a0
Tanvir Tanjum Shourav <39719570+tanvirtanjum@users.noreply.github.com>
2020-07-28 22:53:45 +0600

    T2 MVC Structure implemented

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+var express 	= require('express');
+var app 		= express();
 
+app.get('/', function(req, res){
+	res.send("Go to >> <a href='/login'> LOGIN </a>");
+});
+
+
+app.listen(3333, function(){
+	console.log('Express HTTP Server\nStarted At Port: 3333');
+});
